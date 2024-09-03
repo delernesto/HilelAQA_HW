55ad556a14db70fce308ed44ad1c370485b77e36
delernesto <delernesto832@ukr.net>
2024-09-04 02:03:44 +0300

    1

---
+++ API_HW/FruitsAPI.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Text.Json.Nodes;
+
+
+namespace API_HW
+{
+    internal class FruitsAPI : UITestFixture
+    {
+        [Test]
+        public async Task ReplaceResponse()
+        {
+            await Page.RouteAsync("*/**/api/v1/fruits", async route => {
+                var response = await route.FetchAsync();
+                var body = await response.BodyAsync();
+                var jn = JsonNode.Parse(body);
+                JsonArray ja = jn.AsArray();
+                int indexofOrange = -1;
+                for (int i = 0; i < ja.Count; i++)
+                {
+                    string name = ja[i]["name"].ToString();
+
+                    if (name == "Orange")
+                    {
+                        ja[i]["name"] = "Last fruit";
+                        indexofOrange = i;
+                        break;
+                    }
+                }
+                Assert.That(indexofOrange, Is.GreaterThanOrEqualTo(0));
+
+                for (int i = ja.Count - 1; i > indexofOrange; i--)
+                {
+                    ja.RemoveAt(i);
+                }
+                await route.FulfillAsync(new() { Response = response, Json = ja });              
+            });
+            await Page.GotoAsync("https://demo.playwright.dev/api-mocking");
+            await Assertions.Expect(Page.GetByText("Last fruit")).ToBeVisibleAsync();
+        }
+    }
+}
